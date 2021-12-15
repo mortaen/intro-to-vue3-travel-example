a3ff7c9d494c4b4d04ca343bcf23e5f7fa96d100
mortaen <kolbe.mor@gmail.com>
2021-12-15 17:21:21 +0100

    create components and add data and css for travel destinations app

---
+++ components/Locations.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="place">
+    <img :src="this.img" width="235" height="300" />
+    <h2>{{ name }}</h2>
+    <p>{{ desc }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style scoped>
+.place {
+  display: flex;
+  flex-direction: column;
+  width: 280px;
+  height: 320px;
+  justify-content: center;
+  background: white;
+  border: 1px solid #ddd;
+  padding: 20px 20px;
+  margin: 10px;
+}
+
+h2 {
+  margin: 0;
+  text-align: center;
+}
+img {
+  margin: 10px;
+  align-self: center;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,58 @@
 <template>
-  <Tutorial/>
+  <div>
+    <h1>Your upcoming Destinations</h1>
+    <div class="location-contain">
+      <Locations
+        v-for="location in locations"
+        :key="location"
+        class="locations"
+        :img="location.img"
+        :name="location.name"
+        :desc="location.desc"
+      >
+      </Locations>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      locations: [
+        {
+          name: 'moscow',
+          img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/moscow.svg',
+          desc: `Moscow is the capital and most populous city of Russia, with 13.2 million residents within the city limits and 17.8 million within the urban area. Moscow has the status of a Russian federal city.`,
+        },
+        {
+          name: 'paris',
+          img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/paris1.svg',
+          desc: `Paris is the capital and most populous city of France. By the 17th century, Paris was one of Europe's major centres of finance, commerce, fashion, science, and the arts, and it retains that position still today.`,
+        },
+        {
+          name: 'rome',
+          img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/rome.svg',
+          desc: `Rome's history spans more than 2,500 years. While Roman mythology dates the founding of Rome at around 753 BC, the site has been inhabited for much longer, making it one of the oldest continuously occupied sites in Europe.`,
+        },
+        {
+          name: 'paris',
+          img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/paris2.svg',
+          desc: `By the end of the 12th century, Paris had become the political, economic, religious, and cultural capital of France. Maurice de Sully undertook the construction of the Notre Dame Cathedral at its eastern extremity.`,
+        },
+      ],
+    }
+  },
+}
 </script>
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+
+.location-contain {
+  display: flex;
+  justify-content: center;
+}
+</style>
